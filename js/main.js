f070919f6d4db54f4d25a5dd35f0feb19b525f0a
vitoros9x <68361206+vitoros9x@users.noreply.github.com>
2020-07-28 15:26:18 +0700

    update js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 var x = document.querySelectorAll(".content-text");
+var y = document.querySelectorAll('td');
 
 function appr(a) {
     for (var i = 0; i < x.length; i++) {
-        if (i = a) {
+        if (i == a) {
             x[a].classList.add("active");
+            y[a].classList.add("click");
         } else {
-            x[i].classList.remove("active"," ");
+            x[i].classList.remove("active");
+            y[i].classList.remove("click");
         }
     }
 }
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(document).scrollTop() >2200) {
+            $('.event .number-slide').addClass('number-85');
+            $('.event .slide-bar').addClass('bar-85');
+        }
+    });
+});
