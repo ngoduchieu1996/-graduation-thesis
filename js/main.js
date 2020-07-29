e85b39aadbb3f189bb187417113bdcf460c5168a
vitoros9x <68361206+vitoros9x@users.noreply.github.com>
2020-07-29 18:03:13 +0700

    update

--- js/main.js
+++ js/main.js
@@ -18,98 +18,81 @@ $(document).ready(function () {
         if ($(document).scrollTop() >2000) {
             $('.event .number-slide').addClass('number-85');
             $('.event .slide-bar').addClass('bar-85');
-            percent(85);
+            event();
         }
-    });
-});
 
-$(document).ready(function () {
-    $(window).scroll(function () {
+        function event() {
+            var n = 0;
+            var event = document.getElementById("event");
+            var up = setInterval(increase, 25);
+            function increase() {
+              if (n == 85) {
+                clearInterval(up);
+              } else {
+                n++;
+                event.innerHTML = n + "%";
+              }
+            }
+          }
+
         if ($(document).scrollTop() >2020) {
             $('.food .number-slide').addClass('number-60');
             $('.food .slide-bar').addClass('bar-60');
-            percent(60);
+            food();
         }
-    });
-});
 
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(document).scrollTop() >2020) {
+        function food() {
+            var n = 0;
+            var event = document.getElementById("food");
+            var up = setInterval(increase, 25);
+            function increase() {
+              if (n == 60) {
+                clearInterval(up);
+              } else {
+                n++;
+                event.innerHTML = n + "%";
+              }
+            }
+          }
+
+        if ($(document).scrollTop() >2040) {
             $('.guest .number-slide').addClass('number-75');
             $('.guest .slide-bar').addClass('bar-75');
-            percent(75);
-        }
-    });
-});
+            guest();
 
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(document).scrollTop() >2020) {
-            $('.finding .number-slide').addClass('number-90');
-            $('.finding .slide-bar').addClass('bar-90');
-            percent(90);
-        }
-    });
-});
-
-
-function percent(a) {
-    var n = 0;
-    var event = document.querySelector('.event .number-slide');
-    var food = document.querySelector('.food .number-slide');
-    var guest = document.querySelector('.guest .number-slide');
-    var finding = document.querySelector('.finding .number-slide');
-    var up = setInterval(increase(a), 30);
-
-    switch (a) {
-        case 85:
-            function increase(a) {
-                if (n == a) {
+            function guest() {
+                var n = 0;
+                var event = document.getElementById("guest");
+                var up = setInterval(increase, 25);
+                function increase() {
+                  if (n == 75) {
                     clearInterval(up);
-                } else {
+                  } else {
                     n++;
                     event.innerHTML = n + "%";
+                  }
                 }
+              }
+        }
+
+        if ($(document).scrollTop() >2060) {
+            $('.finding .number-slide').addClass('number-90');
+            $('.finding .slide-bar').addClass('bar-90');
+            finding();
+        }
+
+        function finding() {
+            var n = 0;
+            var event = document.getElementById("finding");
+            var up = setInterval(increase, 25);
+            function increase() {
+              if (n == 90) {
+                clearInterval(up);
+              } else {
+                n++;
+                event.innerHTML = n + "%";
+              }
             }
-            break;
-
-        case 60:
-            function increase(a) {
-                if (n == a) {
-                    clearInterval(up);
-                } else {
-                    n++;
-                    food.innerHTML = n + "%";
-                }
-            }
-            break;
-
-        case 75:
-            function increase(a) {
-                if (n == a) {
-                    clearInterval(up);
-                } else {
-                    n++;
-                    guest.innerHTML = n + "%";
-                }
-            }
-            break;
-
-        case 90:
-            function increase(a) {
-                if (n == a) {
-                    clearInterval(up);
-                } else {
-                    n++;
-                    finding.innerHTML = n + "%";
-                }
-            }
-            break;
-    
-        default:
-            break;
-    }
-}
-
-
+          }
+    });
+});
